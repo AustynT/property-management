96d46fbee054d9125584d62d170ee6b039435e14
AustynT <Austyntravis9@gmail.com>
2018-07-17 14:36:29 -0600

    setup styles and header grid

--- src/components/headerWrapper.js
+++ src/components/headerWrapper.js
@@ -4,8 +4,8 @@ class headerWrapper extends Component{
     render() {
         return(
             <div className='header-wrapper'>
-                <h1>Welcome to HOA manager</h1>
-                <p> please log in to continue</p>
+                <h1 className="header-wrapper__title">Welcome to HOA manager</h1>
+                <p className="header-wrapper__subtitle"> please log in to continue</p>
                 {this.props.children}
             </div>
         )
